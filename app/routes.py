dbc68d4b0291d771b87c15a8a627bec67a1b845a
Juan Carrion <hola@juancrrn.io>
2022-03-14 23:48:55 +0100

    Added test route method

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import App
+from app.models.photo import Photo
 
 flask = App.instance.flask
 
@@ -7,5 +8,14 @@ G_HTTP_NO_CONTENT = 204
 @flask.route('/dispatch')
 def wakeup():
     App.instance.dispatcher.run()
+
+    return ('', G_HTTP_NO_CONTENT)
+
+@flask.route('/testdb')
+def testdb():
+    pending = Photo.db_fetch_pending()
+
+    for item in pending:
+        print(item)
 
     return ('', G_HTTP_NO_CONTENT)
